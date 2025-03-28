b695af0eaa35d678db486f892eddfad33642b889
Xavier MIRABEL <xavier.mirabel@capgemini.com>
2025-03-28 18:05:30 +0100

    debug exe, reste pb tests

--- cmake/CobolFunctions.cmake
+++ cmake/CobolFunctions.cmake
@@ -42,7 +42,7 @@ function(add_cobol_module target_name source_file module_name)
     set(compile_script "${CMAKE_BINARY_DIR}/compile_${target_name}.sh")
     file(WRITE ${compile_script} "#!/bin/bash\n")
     file(APPEND ${compile_script} "cd ${CMAKE_BINARY_DIR}\n")
-    file(APPEND ${compile_script} "${COBC_EXECUTABLE} -m -Wall -std=default -debug -o lib${target_name}.so \"-module-name=${module_name}\" \"${source_file}\"\n")
+    file(APPEND ${compile_script} "${COBC_EXECUTABLE} -m -Wall -std=default -debug -o lib${target_name}.so ${source_file}\n")
     
     # Rendre le script exécutable
     execute_process(COMMAND chmod +x ${compile_script})
